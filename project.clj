7f6ee25d13aa4ffcf128cd6f47a4d17fb783e145
ben lamothe <zonotope@gmail.com>
2017-12-28 18:32:22 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject zonotope/details "0.1.0-SNAPSHOT"
+
+  :description "Common data generators"
+
+  :url "https://github.com/zonotope/details"
+
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/test.check "0.9.0"]])
